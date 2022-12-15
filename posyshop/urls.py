959f3aecc6cefe446b95ac31c0787958559d0e48
Anastasiia Babenko <radiowavezes@gmail.com>
2022-12-16 01:44:40 +0200

    -

--- posyshop/urls.py
+++ posyshop/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
 
+favicon_view = RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('posy.urls', namespace='posy')),
     path('store/', include('store.urls', namespace='store')),
+    re_path(r'^favicon\.ico$', favicon_view),
 ]
